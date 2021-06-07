24732d050936b27a1c2e89884d14115a4cc6010e
iyyel <i@iyyel.io>
2021-06-07 09:54:51 +0200

    safety commit

--- PostScriptGenerator/Library.fs
+++ PostScriptGenerator/Library.fs
@@ -4,15 +4,16 @@ open FPP1.TreeManager
 
 module Generator =
 
-    let moveTo x y = string x + " " + string y + " moveto\n"
-    let text s = "(" + string s + ") " + "show"
-
     let toPSslow tree =
   
         let fm = string 20
         let lh = string 40
 
-        let b = "%!PS\n /Courier \n 20 selectfont \n (Hello world4!) show\n"
+        let beginning = "%!PS \n 700 999 translate \n newpath \n /Courier \n 20 selectfont \n (Hello world4!) show\n"
+
+        let moveTo x y = string x + " " + string y + " moveto\n"
+        let lineto x y = string x + " " + string y + " lineto\n"
+        let label l = "(" + string l + ") " + "show"
 
         let rec genPSTree tree x y =
             match tree with
@@ -22,8 +23,8 @@ module Generator =
             match children with
             | []                                -> ""
             | Node ((label, pos), subtrees)::ts -> ""
-            
-        b + genPSTree tree "0" "0"
+
+        beginning + genPSTree tree "0" "0"
 
     let toPSfast tree =
         ""
