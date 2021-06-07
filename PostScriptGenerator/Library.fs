b5b62fcc8b19869410ae2407dc45451c696eb830
iyyel <i@iyyel.io>
2021-06-07 15:00:53 +0200

    Simplified ps gen

--- PostScriptGenerator/Library.fs
+++ PostScriptGenerator/Library.fs
@@ -14,7 +14,7 @@ module Generator =
         let startX = 0.0
         let startY = -20.0
 
-        let beginning = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n" + string startX + " " + string startY + " moveto\n"
+        let beginning = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
         let endFile = "showpage"
         let stroke = "stroke\n"
 
@@ -27,30 +27,42 @@ module Generator =
             | []                     -> acc * nodeWidth
             | Node ((_, pos), _)::ts -> lineWidth ts ((abs pos) + acc)
 
+
+        let rec genLines children y =
+            match children with
+            | []                     -> ""
+            | Node ((l, pos), _)::ts -> let x = pos * nodeWidth
+                                        let out = moveTo x y + lineto x (y - layerHeight)
+                                        let out = out + genLines ts y
+                                        out
+
         let rec genPSTree tree x y =
             match tree with
             | Node ((l, pos), [])           -> moveTo x y + label l
-            | Node ((l, pos), subtrees)     -> let y = y - lineMargin
-                                               let out = label l + moveTo x y
+            | Node ((l, pos), subtrees)     -> let out = moveTo x y
+                                               let y = y - lineMargin
+                                               let out = out + label l + moveTo x y
                                                let y = y - nodeHeight
                                                let out = out + lineto x y
                                                let lineWidth = lineWidth subtrees 0.
                                                let x = x - (lineWidth / 2.)
                                                let out = out + moveTo x y
                                                let x = x + lineWidth
-                                               let out = out + lineto x y                    
+                                               let out = out + lineto x y           
                                                let out = out + stroke
+                                               let out = out + genLines subtrees y
+                                               let y = y - layerHeight - nodeHeight
                                                out + genPSChildren subtrees startX y
         and genPSChildren children x y =
             match children with
             | []                            -> ""
             | Node ((l, pos), subtrees)::ts -> let x = pos * nodeWidth
-                                               let out = moveTo x y + lineto x (y - layerHeight)
-                                               let out = out + genPSChildren ts x y
-                                               let out = out + List.fold (fun acc t -> acc + genPSTree t x (y - layerHeight - nodeHeight)) "" subtrees
-                                               out + stroke
-                                               
-                                               
+                                               let out = List.fold (fun acc t -> acc + genPSTree t x y) "" ts
+                                               let out = out + List.fold (fun acc t -> acc + genPSTree t x y) "" subtrees
+                                               out
+
+                                                            
+                                              
                                                //out + List.fold (fun acc t -> acc + genPSTree t x (y - layerHeight - nodeHeight)) "" ts + List.fold (fun acc t -> acc + genPSTree t x (y - layerHeight - nodeHeight)) "" subtrees
 
         beginning + genPSTree tree startX startY + endFile
