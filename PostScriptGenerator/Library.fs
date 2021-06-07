3ccb796d0cf21023a9046c9c9ba239c6d605b593
iyyel <i@iyyel.io>
2021-06-08 00:14:34 +0200

    PostScript generation now working well.

--- PostScriptGenerator/Library.fs
+++ PostScriptGenerator/Library.fs
@@ -4,23 +4,27 @@ open FPP1.TreeManager
 
 module Generator =
 
+    let lineMargin = 10.0
+    let nodeHeight = 20.0
+    let layerHeight = 50.0
+    let nodeWidth = 50.0
+
+    let startX = 0.0
+    let startY = -20.0
+
+    let beginning = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
+    let endFile = "showpage"
+    let stroke = "stroke\n"
+
     let toPSslow tree =
- 
-        let lineMargin = 10.0
-        let nodeHeight = 20.0
-        let layerHeight = 50.0
-        let nodeWidth = 50.0
+        let moveto x y = 
+            string x + " " + string y + " moveto\n"
 
-        let startX = 0.0
-        let startY = -20.0
+        let lineto x y = 
+            string x + " " + string y + " lineto\n"
 
-        let beginning = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
-        let endFile = "showpage"
-        let stroke = "stroke\n"
-
-        let moveTo x y = string x + " " + string y + " moveto\n"
-        let lineto x y = string x + " " + string y + " lineto\n"
-        let label l = "(" + string l + ") dup stringwidth pop 2 div neg 0 rmoveto show\n"
+        let label l 
+            = "(" + string l + ") dup stringwidth pop 2 div neg 0 rmoveto show\n"
 
         let comment s = "%% " + s
 
@@ -37,28 +41,28 @@ module Generator =
                                                      x
                                                  else 
                                                      x + pos * nodeWidth 
-                                        let out = moveTo x' y + lineto x' (y - layerHeight)
+                                        let out = moveto x' y + lineto x' (y - layerHeight)
                                         let out = out + genLines ts x y
                                         out
 
 
         let rec genPSTree tree x y =
             match tree with
-            | Node ((l, pos), [])           -> moveTo x y + label l
-            | Node ((l, pos), subtrees)     -> let out = moveTo x y
+            | Node ((l, pos), [])           -> moveto x y + label l
+            | Node ((l, pos), subtrees)     -> let out = moveto x y
                                                let y = y - lineMargin
-                                               let out = out + label l + moveTo x y
+                                               let out = out + label l + moveto x y
                                                let y = y - nodeHeight
                                                let out = out + lineto x y
-                                               let lineWidth = lineWidth subtrees 0.
-                                               let x = x - (lineWidth / 2.)
-                                               let out = out + moveTo x y
+                                               let lineWidth = lineWidth subtrees 0.0
+                                               let x = x - (lineWidth / 2.0)
+                                               let out = out + moveto x y
                                                let x = x + lineWidth
                                                let out = out + lineto x y   
-                                               let x = x - (lineWidth / 2.)
+                                               let x = x - (lineWidth / 2.0)
                                                let out = out + stroke 
                                                let out = out + genLines subtrees x y
-                                               let y = y - layerHeight - nodeHeight
+                                               let y = y - layerHeight - 16.0;
                                                let out = out + genPSChildren subtrees x y
                                                out
         and genPSChildren children x y =
@@ -66,21 +70,15 @@ module Generator =
             | []    -> ""
             | t::ts -> let (Node((_, pos), _)) = t
                        let out = comment ("before: x = " + string x + ", y =  " + string y + ", pos = " + string pos + "\n")
-                       let x = if pos = 0.0 then
-                                   0.0
-                               else 
+                       let x' = if pos = 0.0 then
+                                   x
+                                else 
                                    x + pos * nodeWidth 
                        let out = out + comment ("after: x = " + string x + ", y =  " + string y + "\n")            
-                       let out = out + genPSTree t x y + genPSChildren ts x y 
-                       out        
-
-                                                            
-                                              
-                                               //out + List.fold (fun acc t -> acc + genPSTree t x (y - layerHeight - nodeHeight)) "" ts + List.fold (fun acc t -> acc + genPSTree t x (y - layerHeight - nodeHeight)) "" subtrees
+                       let out = out + genPSTree t x' y + genPSChildren ts x y 
+                       out + stroke        
 
         beginning + genPSTree tree startX startY + endFile
-
-        // + List.fold (fun acc t -> acc + genPSTree t x (y - layerHeight - lineMargin)) "" ts
 
     let toPSfast tree =
         ""
--- Runner/Program.fs
+++ Runner/Program.fs
@@ -3,11 +3,20 @@ open FPP1.TreeManager
 open PostScriptGenerator.Generator
    
 
-let n0 = Node ('A', [Node ('B', [Node ('C', []); Node ('D', []); Node ('E', [])]); Node ('F', [Node ('G', []); Node ('H', []); Node ('I', [])]); Node ('J', [Node ('K', []); Node ('L', []); Node ('M', [])])])
+let n1 = Node ("NODE1", []);
+let n2 = Node ("NODE1", [Node ("NODE2", []); Node ("NODE3", []); Node ("NODE4", [])]);
+let n3 = Node ("NODE1", [Node ("NODE2", [Node ("NODE4", []); Node ("NODE5", [])]); Node ("NODE3", [Node ("NODE6", []); Node ("NODE7", [])])]);
+let n4 = Node ("NODE1", [Node ("NODE2", [Node ("NODE5", []); Node ("NODE6", []); Node ("NODE7", [])]); Node ("NODE3", [Node ("NODE8", []); Node ("NODE9", []); Node ("NODE10", [])]); Node ("NODE4", [Node ("NODE11", []); Node ("NODE12", []); Node ("NODE13", [])])])
+let n5 = Node ("NODE1", [Node ("NODE2", [Node ("NODE6", []); Node ("NODE7", []); Node ("NODE8", []); Node ("NODE9", [Node ("NODE10", []); Node ("NODE11", []); Node ("NODE12", []); Node ("NODE13", [])])]); Node ("NODE3", []); Node ("NODE4", []); Node ("NODE5", [])]);
+let n6 = Node ("NODE1", [Node ("NODE2", [Node ("NODE3", [Node ("NODE4", [Node ("NODE5", []);]);]);]);]);
+let n7 = Node ("NODE1", [Node ("NODE2", []); Node ("NODE3", [Node ("NODE4", []); Node ("NODE5", [Node ("NODE6", []); Node ("NODE7", [])])])]);
+let n8 = Node ("NODE1", [Node ("NODE2", [Node ("NODE4", [Node ("NODE6", []); Node ("NODE7", [])]); Node ("NODE5", [])]); Node ("NODE3", [])]);
+let fact = Node ("Block", [Node ("VarDec", [Node ("n", []); Node ("IntTyp", []); Node ("Int 4", [])]); Node ("VarDec", [Node ("y", []); Node ("IntTyp", []); Node ("Int 1", [])]); Node ("Seq", [Node ("While", [Node ("ApplyPrim", [Node ("<>", []); Node ("ContOf", [Node ("Var \"n\"", [])]); Node ("Int 0", [])]); Node ("Seq", [Node ("PrintLn", [Node ("ApplyPrim", [Node ("toString", []); Node ("ContOf", [Node ("Var \"n\"", [])])])]); Node ("PrintLn", [Node ("ApplyPrim", [Node ("toString", []); Node ("ContOf", [Node ("Var \"y\"", [])])])]); Node ("Ass", [Node ("Var \"y\"", []); Node ("ApplyPrim", [Node ("*", []); Node ("ContOf", [Node ("Var \"n\"", [])]); Node ("ContOf", [Node ("Var \"y\"", [])])])]); Node ("Ass", [Node ("Var \"n\"", []); Node ("ApplyPrim", [Node ("-", []); Node ("ContOf", [Node ("Var \"n\"", [])]); Node ("Int 1", [])])])])]); Node ("PrintLn", [Node ("ApplyPrim", [Node ("toString", []); Node ("ContOf", [Node ("Var \"n\"", [])])])]); Node ("PrintLn", [Node ("ApplyPrim", [Node ("toString", []); Node ("ContOf", [Node ("Var \"y\"", [])])])])])]);
 
 [<EntryPoint>]
 let main argv =
-    let postree = design n0
+    let postree = design fact
+
     let result = toPSslow postree
     printf "%s" result
     0
