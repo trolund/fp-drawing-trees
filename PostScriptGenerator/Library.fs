ae7557ed357e8a62ded98241d0e8802618d7048e
iyyel <i@iyyel.io>
2021-06-08 13:10:14 +0200

    Attempt to beautify PS slow code

--- PostScriptGenerator/Library.fs
+++ PostScriptGenerator/Library.fs
@@ -4,19 +4,21 @@ open FPP1.TreeManager
 
 module Generator =
 
-    let lineMargin = 10.0
-    let nodeHeight = 20.0
-    let layerHeight = 50.0
-    let nodeWidth = 50.0
+    let startX       = 0.0
+    let startY       = -20.0
 
-    let startX = 0.0
-    let startY = -20.0
+    let parentMargin = 10.0
+    let nodeHeight   = 20.0
+    let nodeWidth    = 50.0
+    let depthHeight  = 50.0
+    let depthMargin  = 16.0
 
-    let beginning = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
-    let endFile = "showpage"
-    let stroke = "stroke\n"
+    let psPre        = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
+    let psPost        = "showpage"
+    let stroke       = "stroke\n"
 
-    let toPSslow tree =
+
+    let toPSslow t =
         let moveto x y = 
             string x + " " + string y + " moveto\n"
 
@@ -26,62 +28,53 @@ module Generator =
         let label l 
             = "(" + string l + ") dup stringwidth pop 2 div neg 0 rmoveto show\n"
 
-        let comment s = "%% " + s
+        let positionX x pos =
+            match pos with
+            | 0.0 -> x
+            | _   -> x + pos * nodeWidth
 
-        let rec lineWidth subtrees =
-            match subtrees with 
-            | []                     -> 0.0
-            | Node ((_, pos), _)::[] -> (abs pos) * nodeWidth
-            | Node ((_, pos), _)::ts -> let (Node((_, pos'), _)) = (List.rev ts).Head
-                                        ((abs pos) + (abs pos')) * nodeWidth
+        let rec subtreeWidth ts =
+            match ts with 
+            | []                      -> 0.0
+            | Node ((_, pos), _)::[]  -> abs pos * nodeWidth
+            | Node ((_, pos), _)::ts' -> let (Node((_, pos'), _)) = (List.rev ts').Head
+                                         (abs pos + abs pos') * nodeWidth
                                        
-
-        let rec genLines children x y =
-            match children with
+        let rec subtreeLines ts x y =
+            match ts with
             | []                     -> ""
-            | Node ((l, pos), _)::ts -> let x' = if pos = 0.0 then
-                                                     x
-                                                 else
-                                                     x + pos * nodeWidth 
-                                        let out = moveto x' y + lineto x' (y - layerHeight)
-                                        let out = out + genLines ts x y
+            | Node ((_, pos), _)::ts -> let x'  = positionX x pos 
+                                        let out = moveto x' y + lineto x' (y - depthHeight)
+                                        let out = out + subtreeLines ts x y
                                         out
 
+        let rec psTree t x y =
+            match t with
+            | Node ((l, _), []) -> moveto x y + label l
+            | Node ((l, _), ts) -> let out = moveto x y
+                                   let y = y - parentMargin
+                                   let out = out + label l + moveto x y
+                                   let y = y - nodeHeight
+                                   let out = out + lineto x y
+                                   let lineWidth = subtreeWidth ts
+                                   let x = x - (lineWidth / 2.0)
+                                   let out = out + moveto x y
+                                   let x = x + lineWidth
+                                   let out = out + lineto x y
+                                   let x = x - (lineWidth / 2.0)
+                                   let out = out + stroke
+                                   let out = out + subtreeLines ts x y
+                                   let y = y - depthHeight - depthMargin
+                                   let out = out + psSubtrees ts x y
+                                   out
+        and psSubtrees ts x y =
+            match ts with
+            | []     -> ""
+            | t::ts' -> let (Node((_, pos), _)) = t
+                        let x' = positionX x pos
+                        psTree t x' y + psSubtrees ts' x y + stroke 
+                           
+        psPre + psTree t startX startY + psPost
 
-        let rec genPSTree tree x y =
-            match tree with
-            | Node ((l, pos), [])           -> moveto x y + label l
-            | Node ((l, pos), subtrees)     -> let out = moveto x y
-                                               let y = y - lineMargin
-                                               let out = out + label l + moveto x y
-                                               let y = y - nodeHeight
-                                               let out = out + lineto x y
-                                               let lineWidth = lineWidth subtrees
-                                               let out = out + comment ("lineWidth: " + string lineWidth + "\n")
-                                               let x = x - (lineWidth / 2.0)
-                                               let out = out + moveto x y
-                                               let x = x + lineWidth
-                                               let out = out + lineto x y
-                                               let x = x - (lineWidth / 2.0)
-                                               let out = out + stroke
-                                               let out = out + genLines subtrees x y
-                                               let y = y - layerHeight - 16.0;
-                                               let out = out + genPSChildren subtrees x y
-                                               out
-        and genPSChildren children x y =
-            match children with
-            | []    -> ""
-            | t::ts -> let (Node((_, pos), _)) = t
-                       let out = comment ("before: x = " + string x + ", y =  " + string y + ", pos = " + string pos + "\n")
-                       let x' = if pos = 0.0 then
-                                   x
-                                else 
-                                   x + pos * nodeWidth 
-                       let out = out + comment ("after: x = " + string x + ", y =  " + string y + "\n")            
-                       let out = out + genPSTree t x' y + genPSChildren ts x y 
-                       out + stroke        
-
-        beginning + genPSTree tree startX startY + endFile
-
-    let toPSfast tree =
+    let toPSfast t =
         ""
