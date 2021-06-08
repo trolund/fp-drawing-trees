d1e7a938f3d1c24609c7565646453ab590e699f5
iyyel <i@iyyel.io>
2021-06-08 16:29:42 +0200

    Parsing of AST trees now working.

--- TreeManager/Library.fs
+++ TreeManager/Library.fs
@@ -1,10 +1,77 @@
 ﻿namespace FPP1
 
+open AST
+
 module TreeManager =
 
     type 'a Tree = Node of 'a * ('a Tree list)
     type Extent = (float * float) list
+
+    let n0 = Node(0,[])
+    let n1 = Node(1,[])
+    let n2 = Node(2,[])
+    let n3 = Node(3,[])
+    let n4 = Node(4,[n0;n1])
+    let n5 = Node(5,[n2;n3])
+    let n6 = Node(6,[n4])
+    let n7 = Node(7,[n5])
+    let n8 = Node(8,[n6])
+    let n9 = Node(9,[n7])
+    let n10 = Node(10,[n8;n9])
+
     
+    type Exp =                            
+             | N  of int                     (* Integer constant               *)
+             | B of bool                     (* Boolean constant               *)
+             | Access of Access              (* x    or  ^p    or  a[e]        *)
+             | Addr of Access                (* &x   or  &p^   or  &a[e]       *)
+             | Apply of string * Exp list    (* Function application           *)
+
+    and Access = 
+              | AVar of string               (* Variable access        x       *) 
+              | AIndex of Access * Exp       (* Array indexing         a[e]    *)
+              | ADeref of Exp                (* Pointer dereferencing  p^      *)
+
+    type Stm  =                            
+              | PrintLn of Exp               (* Print                          *) 
+              | Ass of Access * Exp          (* x:=e  or  p^:=e  or  a[e]:=e   *)
+              | Return of Exp option         (* Return from function           *)   
+              | Alt of GuardedCommand        (* Alternative statement          *) 
+              | Do of GuardedCommand         (* Repetition statement           *) 
+              | Block of Dec list * Stm list (* Block: grouping and scope      *)
+              | Call of string * Exp list    (* Procedure call                 *)
+                   
+    and GuardedCommand = GC of (Exp * Stm list) list (* Guarded commands        *)
+
+    and Dec = 
+             | VarDec of Typ * string        (* Variable declaration            *)
+             | FunDec of Typ option * string * Dec list * Stm
+                                             (* Function and procedure declaration *) 
+    and Typ  = 
+             | ITyp                          (* Type int                    *)
+             | BTyp                          (* Type bool                   *)
+             | ATyp of Typ * int option      (* Type array                  *)
+             | PTyp of Typ                   (* Type pointer                *)
+             | FTyp of Typ list * Typ option (* Type function and procedure *)
+
+    type Program = P of Dec list * Stm list  (* Program                     *)
+   
+    let p = P ([VarDec (ITyp, "x")],
+                [Ass (AVar "x", N 1);
+                  Do
+                    (GC
+                       [(Apply ("=", [Access (AVar "x"); N 1]),
+                         [PrintLn (Access (AVar "x"));
+                          Ass (AVar "x", Apply ("+", [Access (AVar "x"); N 1]))]);
+                        (Apply ("=", [Access (AVar "x"); N 2]),
+                         [PrintLn (Access (AVar "x"));
+                          Ass (AVar "x", Apply ("+", [Access (AVar "x"); N 1]))]);
+                        (Apply ("=", [Access (AVar "x"); N 3]),
+                         [PrintLn (Access (AVar "x"));
+                          Ass (AVar "x", Apply ("+", [Access (AVar "x"); N 1]))])]);
+                  PrintLn (Access (AVar "x"))])
+    
+
     let rec same' x y =
         match x,y with
         | Node((_,x'),[]),Node((_,y'),[]) when x'=y'-> true
@@ -12,10 +79,12 @@ module TreeManager =
         | _,_ -> false
     let rec same (Node(_,x)) (Node(_,y)) = List.length x = List.length y && List.forall2 same' x y
 
+
     let rec similar x y =
         match x,y with
         | Node(_,[]), Node(_,[]) -> true
         | Node(_,xs),Node(_,ys) -> List.length xs = List.length ys && List.forall2 similar xs ys
+
 
     let movetree (Node ((label, x), subtrees), x': float) =
         Node((label, x + x'), subtrees)
@@ -93,14 +162,63 @@ module TreeManager =
             (resulttree, resultextent)
         fst (design' tree)
 
-    let n0 = Node(0,[])
-    let n1 = Node(1,[])
-    let n2 = Node(2,[])
-    let n3 = Node(3,[])
-    let n4 = Node(4,[n0;n1])
-    let n5 = Node(5,[n2;n3])
-    let n6 = Node(6,[n4])
-    let n7 = Node(7,[n5])
-    let n8 = Node(8,[n6])
-    let n9 = Node(9,[n7])
-    let n10 = Node(10,[n8;n9])
+
+
+
+
+   
+    let rec parseExp (e:Exp) =
+        match e with
+        | N n           -> Node("Int " + string n, [])
+        | B b           -> Node("Bool " + string b, [])
+        | Access a      -> parseAccess a
+        | Addr a        -> parseAccess a
+        | Apply (s, es) -> Node("Apply " + s, List.map parseExp es)
+     and parseAccess (a:Access) =
+        match a with
+        | AVar s        -> Node("Var " + s, [])
+        | AIndex (a, e) -> Node("Index", [parseAccess a; parseExp e])
+        | ADeref e      -> Node("Deref", [parseExp e])
+
+   
+    let rec parseTyp (t:Typ) =
+        match t with
+        | ITyp           -> Node("IntTyp", [])
+        | BTyp           -> Node("BoolTyp", [])
+        | ATyp (t, opt)  -> match opt with
+                            | Some(i) -> Node("Array", [parseTyp t] @ [Node (string i, [])])
+                            | None    -> Node("Array", [parseTyp t])
+        | PTyp t         -> Node ("Pointer", [parseTyp t])
+        | FTyp (ts, opt) -> match opt with
+                            | Some(t) -> Node("Function", List.map parseTyp ts @ [parseTyp t])
+                            | None    -> Node("Function", List.map parseTyp ts)
+
+
+    let rec parseDec d =
+        match d with
+        | VarDec (t, s)            -> Node ("VarDec", [Node (s, []); parseTyp t])
+        | FunDec (opt, s, ds, stm) -> match opt with
+                                      | Some(t) -> Node("FunDec", [Node(s, [parseTyp t] @ List.map parseDec ds @ [parseStm stm])])
+                                      | None    -> Node("FunDec", [Node(s, List.map parseDec ds @ [parseStm stm])])
+    and parseStm s =
+        match s with
+        | PrintLn e        -> Node("PrintLn", [parseExp e])
+        | Ass (a, e)       -> Node("Ass", [parseAccess a; parseExp e])
+        | Return opt       -> match opt with
+                              | Some(e) -> Node("Return", [parseExp e])
+                              | None    -> Node("Return", [])
+        | Alt gc           -> Node("Alt", [parseGc gc])
+        | Do gc            -> Node("Do", [parseGc gc])
+        | Block (ds, ss)   -> Node("Block", List.map parseDec ds @ List.map parseStm ss)
+        | Call (s, es)     -> Node("Call " + s, List.map parseExp es)
+    and parseGc gc =
+        match gc with
+        | GC ([])          -> Node("GuardedCommand", [])
+        | GC ((e, ss)::gs) -> Node("GuardedCommand", [parseExp e] @ List.map parseStm ss @ [parseGc (GC gs)])
+
+
+
+    let parseProgram p =
+        match p with
+        | P (ds, ss) -> Node ("Program", List.map parseDec ds @ List.map parseStm ss)                       
+
